53a52ea00df6330cee290346225af3ed3abbbec5
Ahmmed Rasel <rahmmed.info@gmail.com>
2023-03-09 01:35:13 +0600

    make responsive

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -7,9 +7,9 @@ const Contact = () => {
 
   return (
     <div>
-      <div className="w-full bg-slate-200 mx-auto p-20">
+      <div className="w-full bg-slate-200 mx-auto lg:p-20 p-4">
         <div className="flex-col">
-          <div className='grid mb-20 grid-cols-3 justify-center'>
+          <div className='grid mb-20 lg:grid-cols-3 gap-8 grid-cols-1 justify-center'>
             <div className='flex flex-col items-center'>
               <BsFillTelephoneFill className='text-5xl text-primary' />
               <h3 className='text-3xl py-3'>Phone</h3>
@@ -29,7 +29,7 @@ const Contact = () => {
             </div>
           </div>
 
-          <div className="card shadow-2xl bg-slate-100 rounded-md mx-24">
+          <div className="card shadow-2xl bg-slate-100 rounded-md lg:mx-24">
             <div className="text-center lg:text-left px-8 mt-8">
               <h1 className="text-5xl font-bold text-primary">Get in touch</h1>
             </div>
@@ -41,7 +41,7 @@ const Contact = () => {
                 </label>
                 <input type="text" placeholder="Full Name" className="input input-bordered" />
               </div>
-              <div className='grid grid-cols-2 gap-8'>
+              <div className='grid lg:grid-cols-2 grid-cols-1 lg:gap-8 gap-2'>
                 <div className="form-control">
                   <label className="label">
                     <span className="label-text">Email</span>
